fd334c4eb7040da7138341b2380e032a77429973
Hakob <hakobeg@gmail.com>
2024-03-11 16:22:34 +0400

    (Add):Featured, featured properties

---
+++ Reservation APP/src/components/propertyList/PropertyList.jsx
@@ -0,0 +1,64 @@
+import * as Styled from "./Styled"
+
+export  const PropertyList = () => {
+  return (
+    <Styled.PropList>
+        <Styled.PropListItem>
+        <img
+          src="https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o="
+          alt=""
+          className="propListImg"
+        />
+         <Styled.PropListTitles>
+            <h1>Hotels</h1>
+            <h2>233 hotels</h2>
+         </Styled.PropListTitles>
+        </Styled.PropListItem>
+        <Styled.PropListItem>
+        <img
+           src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg"
+          alt=""
+          className="propListImg"
+        />
+         <Styled.PropListTitles>
+            <h1>Apartments</h1>
+            <h2>233 hotels</h2>
+         </Styled.PropListTitles>
+        </Styled.PropListItem>
+        <Styled.PropListItem>
+        <img
+          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg"
+          alt=""
+          className="propListImg"
+        />
+         <Styled.PropListTitles>
+            <h1>Resorts</h1>
+            <h2>2331 hotels</h2>
+         </Styled.PropListTitles>
+        </Styled.PropListItem>
+        <Styled.PropListItem>
+        <img
+           src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg"
+          alt=""
+          className="propListImg"
+        />
+         <Styled.PropListTitles>
+            <h1>Villas</h1>
+            <h2>2331 hotels</h2>
+         </Styled.PropListTitles>
+        </Styled.PropListItem>
+        <Styled.PropListItem>
+        <img
+          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg"
+          alt=""
+          className="propListImg"
+        />
+         <Styled.PropListTitles>
+            <h1>Cabins</h1>
+            <h2>2331 hotels</h2>
+         </Styled.PropListTitles>
+        </Styled.PropListItem>
+    </Styled.PropList>
+  )
+
+  }
--- Reservation APP/src/pages/home/Home.jsx
+++ Reservation APP/src/pages/home/Home.jsx
@@ -1,5 +1,8 @@
+import { Featured } from "../../components/featured/Featured"
+import { FeaturedProperties } from "../../components/featured/featuredProperties/FeaturedProperties"
 import { Header } from "../../components/header/Header"
 import { Navbar } from "../../components/navbar/Navbar"
+import { PropertyList } from "../../components/propertyList/PropertyList"
 import * as Styled from "./Styled"
 
 export const Home = () => {
@@ -7,6 +10,13 @@ export const Home = () => {
     <Styled.Home>
       <Navbar />
       <Header />
+      <Styled.HomeContainer>
+        <Featured />
+        <Styled.HomeTitle>Browse by property type</Styled.HomeTitle>
+        <PropertyList/>
+        <Styled.HomeTitle>Homes guests love</Styled.HomeTitle>
+        <FeaturedProperties/>
+      </Styled.HomeContainer>
     </Styled.Home>
   )
 }
